e9a660326279b73cd96b4563220438e9a98b300c
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-08-14 14:16:23 +0200

    add container

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -1,9 +1,10 @@
 <?php
 
 use Core\Database;
+use Core\App;
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
+$db = App::resolve(Database::class);
+
 $currentUser = 5;
 
 $query = "select * from notes where id = :id";
--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -1,9 +1,9 @@
 <?php
 
+use Core\App;
 use Core\Database;
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
+$db = App::resolve(Database::class);
 
 $id = 5;
 $query = "select * from notes where user_id = :id";
