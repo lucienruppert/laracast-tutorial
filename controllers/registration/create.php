55b0fead82e88df4acabcee2f3f014e711a3d7ca
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-08-19 17:01:09 +0200

    show user email in header

--- controllers/registration/create.php
+++ controllers/registration/create.php
@@ -1,9 +1,9 @@
 <?php
 
-// if($_SESSION['user']) {
-//   header('location: /');
-//   exit();
-// }
+if(isset($_SESSION['user'])) {
+  header('location: /');
+  exit();
+}
 
 view('registration/create.view.php', [
   'errors' => [],
